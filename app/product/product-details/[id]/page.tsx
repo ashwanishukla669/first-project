827b5f9c291c466f8078d539d2e2332a17055990
ashwaniomlogic <ashwani.shukla@omlogic.co.in>
2025-04-21 10:25:13 +0530

    fix bugs

--- app/product/product-details/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Product = async ({ params }: { params: { id: string } }) => {
-  const id = params.id;
-  return <div>Product Details {id}</div>;
-};
-
-export default Product;
